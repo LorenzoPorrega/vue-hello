5525413662b5c55a73361e2732772d15bb3e9047
LorenzoPorrega <lorenzoporrega@hotmail.it>
2023-07-19 15:57:41 +0200

    exercise completed

--- js/main.js
+++ js/main.js
@@ -4,9 +4,10 @@ const app = Vue.createApp({
   data(){
     return{
       messaggio: "Hello World!",
-      titleClass: "text-center",
+      titleClass: "text-center mb-5",
       spanClass: "text-danger fs-1 fw-bold",
       link: "https://yt3.googleusercontent.com/UXFC9eFKxjbOcUeEiIFqpywdInXJijIrh5bkfUMPlKhCLKaO6rrTfh5O5IzNTS_2Wap2yBk1J44=s900-c-k-c0x00ffffff-no-rj",
+      imgMsg: "Immagine (e testo presente) printati con Vue ^"
     }
   }
 });
